aa5f0f1f258999114aeaa807bce22650049fcf43
Gerson <gerson.vizcarra@ucsp.edu.pe>
2018-02-26 18:58:09 -0500

    added calibration for chessboard

--- tests/testVideoCalibration.cpp
+++ tests/testVideoCalibration.cpp
@@ -9,7 +9,7 @@
 
 #define WINDOW_ORIGINAL_FRAME "wOriginalFrame"
 #define SAMPLE_TIME 15 // 30fps
-#define VIDEO_FILE_ID "../res/Runcam_HD_2_Calibration_Video.avi"
+#define VIDEO_FILE_ID "../res/calibration_ps3eyecam_checkerboard_4.avi"
 
 int main()
 {
@@ -31,9 +31,9 @@ int main()
     //_videoHandler->setPlaybackAtFrameIndex( 1 );
     //_videoHandler->togglePause();
 
-    int numBoards = 20;
-    int numSquares = 48;
-    cv::Size board_sz = cv::Size(8, 6);
+    int numBoards = 30;
+    int numSquares = 54;
+    cv::Size board_sz = cv::Size(9, 6);
 
     vector< vector< cv::Point3f > > object_points;
     vector< vector< cv::Point2f > > image_points;
@@ -41,23 +41,29 @@ int main()
     int successes = 0;
 
     vector< cv::Point3f > obj;
-    for( int j = 0; j < numSquares; j++ )
-        obj.push_back( cv::Point3f( j / 8, j % 6, 0.0f ) );
-
+    /*for( int j = 0; j < numSquares; j++ )
+        obj.push_back( cv::Point3f( 24 * (j / 9), 24 * (j % 6), 0.0f ) );*/
 
     cv::Mat _frame;
     cv::Mat gray_image;
     _videoHandler->takeFrame( _frame );
 
+    for( int i = 0; i < 6; i++ )
+        for( int j = 0; j < 9; j++ )
+            obj.push_back( cv::Point3f( 24 * i, 24 * j, 0.0f ) );
+
+
+
     while( successes<numBoards )
     {
         cv::cvtColor( _frame, gray_image, CV_BGR2GRAY );
-        bool found = cv::findChessboardCorners( gray_image, board_sz, corners,
-                                                CV_CALIB_CB_ADAPTIVE_THRESH + CV_CALIB_CB_NORMALIZE_IMAGE
-                                                + CV_CALIB_CB_FAST_CHECK);
+        bool found = cv::findChessboardCorners( _frame, board_sz, corners,
+                                                CV_CALIB_CB_ADAPTIVE_THRESH | CV_CALIB_CB_FILTER_QUADS );
         if(found)
         {
-            cv::cornerSubPix(gray_image, corners, cv::Size(11, 11), cv::Size(-1, -1), cv::TermCriteria(CV_TERMCRIT_EPS | CV_TERMCRIT_ITER, 30, 0.1));
+            //cv::cornerSubPix(gray_image, corners, cv::Size(5, 5), cv::Size(-1, -1), cv::TermCriteria(CV_TERMCRIT_EPS | CV_TERMCRIT_ITER, 30, 0.1));
+            cv::cornerSubPix( gray_image, corners, cv::Size(11,11),
+                        cv::Size(-1,-1), cv::TermCriteria( cv::TermCriteria::EPS + cv::TermCriteria::COUNT, 30, 0.1 ));
             cv::drawChessboardCorners(gray_image, board_sz, corners, found);
         }
 
@@ -65,7 +71,7 @@ int main()
         cv::imshow( "Calibration Test", gray_image );
 
         _videoHandler->takeFrame( _frame );
-        int key = cv::waitKey(1) & 0xff;
+        int key = cv::waitKey(15) & 0xff;
         //cout<<key<<endl;
         if( key == KEY_ESCAPE )
             return 0;
@@ -88,11 +94,11 @@ int main()
     vector<cv::Mat> rvecs;
     vector<cv::Mat> tvecs;
 
+    //intrinsic.ptr<float>(0)[0] = _frame.size().width / _frame.size().height;
     intrinsic.ptr<float>(0)[0] = 1;
     intrinsic.ptr<float>(1)[1] = 1;
-    //intrinsic.ptr<float>(1)[1] = 1;
-    cv::calibrateCamera(object_points, image_points, _frame.size(), intrinsic, distCoeffs, rvecs, tvecs);
-
+    double rms = cv::calibrateCamera(object_points, image_points, _frame.size(), intrinsic, distCoeffs, rvecs, tvecs);
+    cout<<"Reprojection Error: "<<rms<<endl;
     cv::Mat imageUndistorted;
     while(1)
     {
@@ -102,8 +108,16 @@ int main()
         cv::imshow(WINDOW_ORIGINAL_FRAME, _frame);
         cv::imshow("Calibration Test", imageUndistorted);
         int key = cv::waitKey(1);
-        if( key == KEY_ESCAPE )
-            return 0;
+        int _key = cv::waitKey( SAMPLE_TIME ) & 0xff;
+
+        if ( _key == ' ' )
+        {
+            _videoHandler->togglePause();
+        }
+        else if ( _key == KEY_ESCAPE )
+        {
+            break;
+        }
     }
 
     /*while( 1 )
